3e5911bc2cdb45eac4f065d8296c71a4a88aafb0
Jurandy Soares <jurandy.soares@gmail.com>
2023-11-06 14:31:56 -0300

    feat: Versão concorrente de colario

--- concolario.py
+++ concolario.py
@@ -4,9 +4,9 @@
 import argparse
 import asyncio
 import glob
+import logging
 import os
 import pathlib
-import subprocess
 import tempfile
 
 # Singular da categoria
@@ -17,38 +17,45 @@ sing_categ = {
 }
 
 cmd_pdf2 = {
-    'txt': 'pdftotext -layout {nome_arq_pdf} ../{ext}/{nome_arq_pdf[:-4]}.txt',
-    'svg': 'pdf2svg {nome_arq_pdf} ../{ext}/{nome_arq_pdf[:-4]}.svg',
-    'png': 'pdftoppm -png -singlefile {nome_arq_pdf} ../{ext}/{nome_arq_pdf[:-4]}'
+    'txt': 'pdftotext {cam_pdf} {cam_ext}',
+    'svg': 'pdf2svg {cam_pdf} {cam_ext}',
+    'png': 'pdftoppm -png -singlefile {cam_pdf} {cam_ext}'
 }
 
+async def disparar(comando: str):
+    processo = await asyncio.subprocess.create_subprocess_shell(comando)
+    stdout,stdin = await processo.communicate()
+    await processo.wait()
 
-async def converter_pdf(arquivo_pdf, ext: str):
-    comando = cmd_pdf2[ext].format(arquivo_pdf, ext)
-    print(comando)
-    subprocess.run(comando.split())
 
 async def separar_pdf(nome_categoria: str, dir_dest_pdf: pathlib.Path):
     dir_pdf_categoria = dir_dest_pdf/f'{nome_categoria}'
     dir_pdf_categoria.mkdir(parents=True, exist_ok=True)
     padrao_caminho = dir_pdf_categoria/f'{nome_categoria}-%02d.pdf'
 
-    comando = f'pdfseparate {nome_categoria}.pdf {padrao_caminho}'
-    print(comando)
-    subprocess.run(comando.split())
-    print(f'Separação de "{nome_categoria}.pdf" concluída.')
+    comando = f'/usr/bin/pdfseparate {nome_categoria}.pdf {padrao_caminho}'
+    await disparar(comando)
 
-    arqs_pdf = glob.glob('*.pdf')
-    arqs = [arq_pdf[:-4] for arq_pdf in arqs_pdf]
-    for ext in 'txt svg png'.split():
-        dir_dest_ext = dir_dest_pdf.parent/f'ext'
-        dir_dest_ext.mkdir(parents=True, exist_ok=True)
-        async with asyncio.TaskGroup() as tg:
-            conversores_ext = [
-                asyncio.create_task(converter_pdf(arq_pdf, ext))
-                for arq_pdf in arqs_pdf
-            ]
+async def converter_pdf(ext: str, dir_dest_pdf: pathlib.Path):
+    dir_ext = dir_dest_pdf.parent/ext
+    dir_ext.mkdir(exist_ok=True, parents=True)
+    for categoria in 'professor sala turma'.split():
+        dir_ext_categ = dir_ext/categoria
+        dir_ext_categ.mkdir(parents=True, exist_ok=True)
 
+    pdfs = dir_dest_pdf.glob('**/*.pdf')
+    caminhos = [str(r) for r in pdfs]
+    comandos = []
+    mod_comando = cmd_pdf2[ext]
+    for cam in caminhos:
+         comandos.append(mod_comando.format(cam_pdf=cam, cam_ext=cam.replace("pdf", ext)))
+    
+    if ext=='png':
+        tarefas = [disparar(cmd[:-4]) for cmd in comandos]
+    else:
+        tarefas = [disparar(cmd) for cmd in comandos]
+
+    await asyncio.gather(*tarefas)
 
 async def main():
     ana_args = argparse.ArgumentParser()
@@ -56,18 +63,27 @@ async def main():
                           help='Diretório com arquivos PDF')
     args = ana_args.parse_args()
 
-    global pdf_dir
     pdf_dir = pathlib.Path(args.diretorio_pdf)
+
     if pdf_dir.exists and pdf_dir.is_dir():
         os.chdir(pdf_dir)
-        print(os.getcwd())
         categorias = 'professor sala turma'.split()
-        dir_pdf = pathlib.Path(tempfile.mkdtemp('-concolario'))/'pdf'
-        dir_pdf.mkdir(parents=True, exist_ok=True)
-        async with asyncio.TaskGroup() as tg:
-            sepadores_pdf = [
+        with tempfile.TemporaryDirectory(prefix='concolario-') as dir_temp:
+            print(f'Diretório temporário: {dir_temp}')
+            dir_pdf = pathlib.Path(dir_temp)/'pdf'
+            dir_pdf.mkdir(parents=True, exist_ok=True)
+
+            tarefas = sepadores_pdf = [
                 asyncio.create_task(separar_pdf(categoria, dir_pdf))
                 for categoria in categorias
             ]
+            await asyncio.gather(*tarefas)
+            await converter_pdf(ext='txt', dir_dest_pdf=dir_pdf)
+            await converter_pdf(ext='svg', dir_dest_pdf=dir_pdf)
+            await converter_pdf(ext='png', dir_dest_pdf=dir_pdf)
+            
+            print('Separação terminada.')
+            input('Pressione ENTER')
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
